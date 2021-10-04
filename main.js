d2c9025608bbe866dd8f0d2d35215353bdea15c7
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-10-04 11:46:31 -0400

    Update main.js

--- main.js
+++ main.js
@@ -80,11 +80,7 @@ class BasicWorldDemo {
 
     this._scene.background = texture;
 
-    const plane = new THREE.Mesh(
-        new THREE.PlaneGeometry(100, 100, 10, 10),
-        new THREE.MeshStandardMaterial({
-            color: 0xFFFFFF,
-          }));
+
     plane.castShadow = false;
     plane.receiveShadow = true;
     plane.rotation.x = -Math.PI / 2;
